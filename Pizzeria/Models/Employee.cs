ebb0b22ff173179dfd6a78b9b9ebfab5964de7ca
karichil <karina.chilkiewicz@gmail.com>
2024-10-22 13:00:41 +0200

    atributes for models

--- Pizzeria/Models/Employee.cs
+++ Pizzeria/Models/Employee.cs
@@ -1,7 +1,10 @@
-﻿namespace Pizzeria.Models
+﻿using System;
+
+namespace Pizzeria.Models
 {
     public class Employee:User
     {
-        
+        public long Id { get; set; }
+        public DateTime DateoOfEmployment { get; set; }
     }
 }
